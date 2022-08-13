8c1847c296bdd2402b66bd5442239d10303f6c51
daryatalantseva <d.talantseva@gmail.com>
2022-08-13 09:43:21 +0300

    Задачи семинар 3

---
+++ sem3_pr2/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+A (3,6,8); B (2,1,-7), -> 15.84
+
+A (7,-5, 0); B (1,-1,9) -> 11.53 */
+Console.WriteLine("Введите координаты двух точек в пространстве x1, y, z1, x2, y2, z2");
+double x1 = Convert.ToInt32(Console.ReadLine());
+double y1 = Convert.ToInt32(Console.ReadLine());
+double z1 = Convert.ToInt32(Console.ReadLine());
+double x2 = Convert.ToInt32(Console.ReadLine());
+double y2 = Convert.ToInt32(Console.ReadLine());
+double z2 = Convert.ToInt32(Console.ReadLine());
+	
+double d = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2));
+Console.WriteLine("Расстояние между заданными точками равно " + d);
